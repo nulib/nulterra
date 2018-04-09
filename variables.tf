142471134ce53620c0c2a363bf26e7db9fdf9e93
toputnal <james.bottino@northwestern.edu>
2018-04-09 11:24:48 -0500

    Move AWS credentials to variables.cf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,11 @@ variable "aws_region"          {
   default = "us-east-1"
 }
 
-variable "stack_name"          { type = "string" }
-variable "project_name"        { type = "string" }
-variable "hosted_zone_name"    { type = "string" }
-variable "ec2_keyname"         { type = "string" }
-variable "ec2_private_keyfile" { type = "string" }
-variable "tags"                { type = "map"    }
+variable "stack_name"          		{ type = "string" }
+variable "project_name"        		{ type = "string" }
+variable "hosted_zone_name"    		{ type = "string" }
+variable "ec2_keyname"         		{ type = "string" }
+variable "ec2_private_keyfile" 		{ type = "string" }
+variable "tags"                		{ type = "map"    }
+variable "aws_access_key_id"   		{ type = "string" }
+variable "aws_secret_access_key"	{ type = "string" }
