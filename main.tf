695952b6d36b32ca5ae87c25618739fbf98d836c
toputnal <james.bottino@northwestern.edu>
2018-04-09 12:00:47 -0500

    Moved providers options to main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  region = "${var.AWS_REGION}"
 }
 
 module "stack" {
--- variables.tf
+++ variables.tf
@@ -1,13 +1,27 @@
-variable "aws_region"          {
+variable "AWS_REGION"          {
   type = "string"
   default = "us-east-1"
 }
 
-variable "stack_name"          		{ type = "string" }
+resource "aws_instance" "example" {
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  }
+
+variable "stack_name"          		{ type = "string"}
 variable "project_name"        		{ type = "string" }
 variable "hosted_zone_name"    		{ type = "string" }
 variable "ec2_keyname"         		{ type = "string" }
 variable "ec2_private_keyfile" 		{ type = "string" }
 variable "tags"                		{ type = "map"    }
-variable "aws_access_key_id"   		{ type = "string" }
-variable "aws_secret_access_key"	{ type = "string" }
+variable "AWS_ACCESS_KEY_ID"   		{ type = "string" }
+variable "AWS_SECRET_ACCESS_KEY"	{ type = "string" }
+
+variable "AMIS" {
+  type = 'map'
+  default = {
+    us-east-1 = "ami-1853ac65"
+    us-east-2 = "ami-25615740"
+    us-west-2 = "ami-d874e0a0"
+  }
+}
