f5e03d83b4c4fff5ce8090ac649307e67f60d892
toputnal <james.bottino@northwestern.edu>
2018-04-20 14:32:59 -0500

    backed out of using terraform ecs module

--- modules/stack/arch_web.tf
+++ modules/stack/arch_web.tf
@@ -1,4 +1,3 @@
-
 resource "aws_iam_user" "arch_web_bucket_user" {
   name = "${local.namespace}-arch_web"
   path = "/system/"
@@ -16,10 +15,4 @@ resource "aws_route53_record" "arch_web" {
   zone_id = "${module.dns.private_zone_id}"
   name    = "arch_web.${local.private_zone_name}"
   type    = "A"
-
-  alias {
-    name                   = "${module.fcrepo_environment.elb_dns_name}"
-    zone_id                = "${module.fcrepo_environment.elb_zone_id}"
-    evaluate_target_health = true
-  }
 }
