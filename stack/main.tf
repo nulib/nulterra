b961bea684c4526bcd9f481e7fe29e3f0f038ae3
Michael Klein <mbklein@gmail.com>
2018-05-17 23:23:14 -0500

    Don't version the app sources bucket

--- stack/main.tf
+++ stack/main.tf
@@ -14,19 +14,5 @@ data "aws_elastic_beanstalk_solution_stack" "multi_docker" {
 resource "aws_s3_bucket" "app_sources" {
   bucket = "${local.namespace}-sources"
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    prefix = "/"
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-
   tags   = "${local.common_tags}"
 }
