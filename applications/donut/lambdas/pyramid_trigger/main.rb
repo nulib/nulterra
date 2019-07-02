52110fa1430325add624ad6f9038ca2fa64b1cf8
Michael Klein <mbklein@gmail.com>
2019-07-02 16:31:31 -0500

    Better error handling for pyramid trigger lambda

--- applications/donut/lambdas/pyramid_trigger/main.rb
+++ applications/donut/lambdas/pyramid_trigger/main.rb
@@ -17,20 +17,34 @@ class LambdaHandler
     @context = context
     @pyramid_queue_url = ENV['QueueUrl']
     @ecs = Task.new
-    @pyramid_bucket = event['target'].split(/s3:\/\/(.*?)\//)[1]
-    @destination_s3_key = event['target'].scan(/^s3:\/\/#{pyramid_bucket}\/(.+)$/).flatten.first
+    unless event['target'].nil?
+      @pyramid_bucket = event['target'].split(/s3:\/\/(.*?)\//)[1]
+      @destination_s3_key = event['target'].scan(/^s3:\/\/#{pyramid_bucket}\/(.+)$/).flatten.first
+    end
   end
 
   def process!
-    return { statusCode: 304, body: JSON.generate('Not Modified') } if destination_exists?
     begin
+      if event['target'].nil?
+        return { statusCode: 200, body: JSON.generate('OK') } if ecs.task_running? 
+        ecs.run_task if messages_waiting?
+        { statusCode: 201, body: JSON.generate('Started...') }
+      end
+      return { statusCode: 304, body: JSON.generate('Not Modified') } if destination_exists?
+    
       send_to_sqs
       ecs.run_task unless ecs.task_running?
       { statusCode: 201, body: JSON.generate('Created') }
     rescue StandardError => error
       puts error.backtrace.join("\n")
-      { statusCode: 500, body: JSON.generate(error.message) }
+      { statusCode: 500, body: error.message }
     end
+  end
+  
+  def messages_waiting?
+    sqs = Aws::SQS::Client.new
+    req = sqs.get_queue_attributes( { queue_url: pyramid_queue_url, attribute_names: ['ApproximateNumberOfMessages'] })
+    req.attributes['ApproximateNumberOfMessages'].to_i.positive?
   end
 
   def send_to_sqs
