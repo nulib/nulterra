5b004fb766b4227e6115bf76766061942784d601
toputnal <james.bottino@northwestern.edu>
2019-07-10 14:30:30 -0500

    Added tagging to lambdas redis and solrcloud

--- stack/redis.tf
+++ stack/redis.tf
@@ -25,6 +25,7 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = "default.redis5.0"
   security_group_ids   = ["${aws_security_group.redis.id}"]
   subnet_group_name    = "${aws_elasticache_subnet_group.redis.name}"
+  tags                 = "${merge(local.common_tags, map("Name", "${local.namespace}-redis"))}"
 }
 
 resource "aws_route53_record" "redis" {
