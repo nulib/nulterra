023848ca6aee2871b3c45aa31dec902f6938399d
Karen Shaw Didrickson <karendid@gmail.com>
2020-05-19 10:51:14 -0500

    Search both meadow and common index for FileSet visibility

--- stack/lambdas/iiif/lib/authorize.js
+++ stack/lambdas/iiif/lib/authorize.js
@@ -1,16 +1,16 @@
-const isObject = require('lodash.isobject');
-const isString = require('lodash.isstring');
-const fetch    = require('node-fetch');
-const jwt      = require('jsonwebtoken');
-const url      = require('url');
-const AWS      = require('aws-sdk');
+const isObject = require("lodash.isobject");
+const isString = require("lodash.isstring");
+const fetch = require("node-fetch");
+const jwt = require("jsonwebtoken");
+const url = require("url");
+const AWS = require("aws-sdk");
 
 const apiTokenSecret = process.env.api_token_secret;
-const elasticSearch  = process.env.elastic_search;
-const allowedFrom    = allowedFromRegexes(process.env.allow_from);
+const elasticSearch = process.env.elastic_search;
+const allowedFrom = allowedFromRegexes(process.env.allow_from);
 
 function allowedFromRegexes(str) {
-  var configValues = isString(str) ? str.split(';') : [];
+  var configValues = isString(str) ? str.split(";") : [];
   var result = [];
   for (var re in configValues) {
     result.push(new RegExp(configValues[re]));
@@ -22,7 +22,7 @@ function getCurrentUser(token) {
   if (isString(token)) {
     try {
       return jwt.verify(token, apiTokenSecret);
-    } catch(err) {
+    } catch (err) {
       return null;
     }
   } else {
@@ -33,17 +33,25 @@ function getCurrentUser(token) {
 async function fetchJson(request) {
   return new Promise((resolve, reject) => {
     var client = new AWS.HttpClient();
-    client.handleRequest(request, null, (response) => {
-      var responseBody = '';
-      response.on('data', (chunk) => { 
-        responseBody += chunk; 
-      });
-      response.on('end', () => { 
-        response.body = responseBody;
-        response.json = JSON.parse(responseBody);
-        resolve(response);
-      });
-    }, (error) => reject(error));
+    client.handleRequest(
+      request,
+      null,
+      (response) => {
+        var responseBody = "";
+        response.on("data", (chunk) => {
+          responseBody += chunk;
+        });
+        response.on("end", () => {
+          response.body = responseBody;
+          response.json = JSON.parse(responseBody);
+          resolve(response);
+        });
+      },
+      (error) => {
+        console.log("ERROR RETRIEVING AUTH DOCUMENT: ", error);
+        resolve(null);
+      }
+    );
   });
 }
 
@@ -52,16 +60,16 @@ async function makeRequest(method, requestUrl, body = null) {
     var chain = new AWS.CredentialProviderChain();
     var request = new AWS.HttpRequest(requestUrl, AWS.config.region);
     request.method = method;
-    request.headers['Host'] = url.parse(requestUrl).host;
+    request.headers["Host"] = url.parse(requestUrl).host;
     request.body = body;
-    request.headers['Content-Type'] = 'application/json';
+    request.headers["Content-Type"] = "application/json";
 
-    chain.resolve((err, credentials) => { 
+    chain.resolve((err, credentials) => {
       if (err) {
-        console.log('WARNING: ', err);
-        console.log('Returning unsigned request');
+        console.log("WARNING: ", err);
+        console.log("Returning unsigned request");
       } else {
-        var signer = new AWS.Signers.V4(request, 'es');
+        var signer = new AWS.Signers.V4(request, "es");
         signer.addAuthorization(credentials, new Date());
       }
       resolve(request);
@@ -75,16 +83,29 @@ async function authorize(token, id, referer) {
   }
 
   var currentUser = getCurrentUser(token);
-  var docUrl = url.parse(url.resolve(elasticSearch, `common/_doc/${id}`));
-  var request = await makeRequest('GET', docUrl);
+  var query = {
+    query: {
+      match: { _id: id },
+    },
+  };
+
+  var docUrl = url.parse(url.resolve(elasticSearch, `_all/_search`));
+  var request = await makeRequest("POST", docUrl, JSON.stringify(query));
   var response = await fetchJson(request);
   var doc = response.json;
 
-  if (isObject(doc._source) && isString(doc._source.visibility)) {
-    switch(doc._source.visibility) {
-      case 'open':          return true;
-      case 'authenticated': return isObject(currentUser);
-      case 'restricted':    return false;
+  if (
+    isObject(doc.hits) &&
+    doc.hits.total >= 1 &&
+    isString(doc.hits.hits[0]._source.visibility)
+  ) {
+    switch (doc.hits.hits[0]._source.visibility.toLowerCase()) {
+      case "open":
+        return true;
+      case "authenticated":
+        return isObject(currentUser);
+      case "restricted":
+        return false;
     }
   }
   return false;
